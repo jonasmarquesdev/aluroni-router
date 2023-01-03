975f5dc72da900699cb4dd491316955ef1558eea
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-01-03 13:56:13 -0300

    Resolvendo rotas js

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,11 @@ import 'normalize.css';
 import './index.css';
 import Cardapio from './pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import Router from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Inicio />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
