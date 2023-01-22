334be818a36cc5f1a8c2f30a05aba949e551595a
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-01-22 08:12:39 -0300

    Criando página Sobre

---
+++ src/pages/Sobre/index.tsx
@@ -0,0 +1,7 @@
+import styles from './Sobre.module.scss';
+
+export default function Sobre() {
+  return(
+    <section>Sobre</section>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import Menu from 'components/Menu';
 import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import Sobre from 'pages/Sobre';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -13,7 +14,8 @@ export default function AppRouter() {
         <Routes>
           <Route path='/' element={<PaginaPadrao />}>
             <Route index element={<Inicio />}/>
-            <Route path='cardapio' element={<Cardapio />} />            
+            <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
           </Route>
         </Routes>
       </Router>
