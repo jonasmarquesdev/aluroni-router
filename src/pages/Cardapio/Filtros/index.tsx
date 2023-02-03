6cf4b6adba00dc1812b849c826f5af126377f283
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-02-03 08:41:15 -0300

    Adicionando comentários ao Filtros

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -5,6 +5,7 @@ import classNames from 'classnames';
 
 type IOpcao = typeof filtros[0];
 
+// Recebendo o estado filtro do Cardapio
 interface Props {
   filtro: number | null;
   setFiltro: React.Dispatch<React.SetStateAction<number | null>>
@@ -12,17 +13,19 @@ interface Props {
 
 export default function Filtros({ filtro, setFiltro }: Props) {
 
-  function selecionarFiltro(opcao: IOpcao) {
-    if (filtro === opcao.id) return setFiltro(null);
-    return setFiltro(opcao.id);
+  function selecionarFiltro(opcao: IOpcao) { // Explicando que opcao é um tipo IOpcao
+    if (filtro === opcao.id) return setFiltro(null); // Desseleciona o filtro se o if for true
+    return setFiltro(opcao.id); // Passa para o estado filtro o filtro celecionado pelo id do filtro
   }
+
   return (
     <div className={styles.filtros}>
       {filtros.map(opcao => (
         <button 
+          // Utilizando o classNames para não precisar concatenar os styles
           className={classNames({
             [styles.filtros__filtro]: true,
-            [styles['filtros__filtro--ativo']]: filtro === opcao.id
+            [styles['filtros__filtro--ativo']]: filtro === opcao.id // É necessário usar o [''] por que sem não reconhece o --
           })} 
           key={opcao.id} 
           onClick={() => selecionarFiltro(opcao)}
